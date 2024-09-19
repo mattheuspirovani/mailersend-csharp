f5ec19ab73e6c9a4c222af0f08d26ffb3c5256c3
Dev01 <hello@atrixian.com.br>
2024-09-18 23:07:43 -0300

    Update the IActivityClient to make ActivityListOptions nullable

--- src/MailerSend.Sdk/Activities/IActivityClient.cs
+++ src/MailerSend.Sdk/Activities/IActivityClient.cs
@@ -4,7 +4,7 @@ namespace MailerSend.Sdk.Activities;
 
 public interface IActivityClient
 {
-    Task<ActivityListResponse?> GetActivitiesAsync(ActivityListOptions options);
+    Task<ActivityListResponse?> GetActivitiesAsync(ActivityListOptions? options = null);
     Task<Activity?> GetActivityAsync(string id);
 
 }
