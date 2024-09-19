218d6f435a2e6b458460402ed9a48d8fe81b701e
Dev01 <hello@atrixian.com.br>
2024-09-19 08:58:27 -0300

    Update to expose the DomainClient

--- src/MailerSend.Sdk/MailerSendClient.cs
+++ src/MailerSend.Sdk/MailerSendClient.cs
@@ -1,4 +1,5 @@
 using MailerSend.Sdk.Activities;
+using MailerSend.Sdk.Domains;
 
 namespace MailerSend.Sdk;
 
@@ -6,9 +7,11 @@ public class MailerSendClient
 {
     //public IEmailClient Email { get; }
     public IActivityClient Activity { get; }
+    public IDomainsClient Domains { get; }
 
     public MailerSendClient(string apiKey, HttpClient? httpClient = null)
     {
         Activity = new ActivityClient(apiKey, httpClient);
+        Domains = new DomainClient(apiKey, httpClient);
     }
 }
