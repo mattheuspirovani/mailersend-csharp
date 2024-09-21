24e0da83fb8a1ca15fe87e829aa85b6dbacb38d3
Dev01 <hello@atrixian.com.br>
2024-09-20 23:26:44 -0300

    Fix indentation

--- src/MailerSend.Sdk/Domains/IDomainsClient.cs
+++ src/MailerSend.Sdk/Domains/IDomainsClient.cs
@@ -3,8 +3,8 @@ using System;
 namespace MailerSend.Sdk.Domains;
 
 public interface IDomainsClient
-    {
-        Task<DomainListResponse?> GetDomainsAsync(DomainsListOptions? options = null);
-        Task<Domain?> GetDomainAsync(string domainId);
-        Task<Domain?> CreateDomainAsync(CreateDomainRequest request);
-    }
+{
+    Task<DomainListResponse?> GetDomainsAsync(DomainsListOptions? options = null);
+    Task<Domain?> GetDomainAsync(string domainId);
+    Task<Domain?> CreateDomainAsync(CreateDomainRequest request);
+}
